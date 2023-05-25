72a1393f671834dcd5336ea66325d443c48dae3d
AshanCoonghe <134504785+AshanCoonghe@users.noreply.github.com>
2023-05-25 21:50:51 +0530

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Pearl Morais/AppData/Local/Programs/Python/Python311/myfirstprogram.py
+Hello, World!
+what is your name? Ashan
+Hello  Ashan
+>>> 
+>>> age=12
+>>> print(age)
+12
+>>> 
+>>> weight=36
+>>> print(weight)
+36
+>>> 
+>>> height=149
+>>> print(height)
+149
